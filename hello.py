84bb0e663233b180cdcdb82b651059864b1ff6df
GeorgySerga <georgy.serga@gmail.com>
2016-11-15 14:07:45 +0300

    Fix 1.9.11

--- hello.py
+++ hello.py
@@ -1,13 +1,7 @@
-from cgi import parse_qs
-
-
 def application(env, start_response):
     status = '200 OK'
     headers = [('Content-type', 'text/plain')]
     start_response(status, headers)
-    request_query = parse_qs(env['QUERY_STRING'])
-    response = ''
-    for key in request_query:
-        for value in request_query[key]:
-            response += str(key) + '=' + str(value) + '\n'
-    return [response]
+    qs = env['QUERY_STRING']
+    data = '\n'.join(qs.split('&'))
+    return [data]
