374729b45e432456edb73d6cb22ba1555742c72c
GeorgySerga <georgy.serga@gmail.com>
2016-11-22 17:04:56 +0300

    2.3.10

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,33 @@
 from django.http import HttpResponse
+from qa.models import Question
+from django.core.paginator import Paginator
+from django.core.urlresolvers import reverse
 
  
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+def new(request):
+    qs = Question.objects.all()
+    qs = qs.order_by('-added_at')
+    page, paginator = paginate(request, qs)
+    paginator.baseurl = reverse('question_list') + '?page='
+
+    return render(request, 'list.html', {
+        'questions': page.object_list,
+        'page': page,
+        'paginator': paginator,
+    })
+
+
+def popular(request):
+    qs = Question.objects.all()
+    qs = qs.order_by('-rating')
+    page, paginator = paginate(request, qs)
+    paginator.baseurl = reverse('popular') + '?page='
+
+    return render(request, 'list_rating.html', {
+        'questions': page.object_list,
+        'page': page,
+        'paginator': paginator,
+    })
--- init.sh
+++ init.sh
@@ -4,3 +4,8 @@ sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
+
+sudo /etc/init.d/mysql start
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS stepik CHARACTER SET utf8;"
+mysql -u root -e "CREATE USER 'stepik'@'localhost' IDENTIFIED BY 'stepik';"
+mysql -u root -e "GRANT ALL ON stepik.* TO 'stepik'@'localhost';"
