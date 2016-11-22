720481563a863e7fdbae20833d646bec60a6f76e
GeorgySerga <georgy.serga@gmail.com>
2016-11-22 18:30:02 +0300

    Fix 2, 2.3.10

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class QuestionManager():
+class QuestionManager(models.Manager):
     def new(self):
         self.oder_by('added_at')
     def popular():
