e2ecae58ef1d476879cb4bbe97d77f3832af74d9
Georgy <georgy.serga@gmail.com>
2016-11-24 18:56:29 +0300

    2.5.8

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls import url
-from qa.views import test
+from qa.views import test, question_details, question_list
 
 urlpatterns = [
-    url(r'^$', test),                                                              
-    url(r'^login/.*$', test, name='login'),                                    
-    url(r'^signup/.*', test, name='signup'),                                   
-    url(r'^question/(?P<id>[0-9]+)/$', test, name='question'),                 
-    url(r'^ask/.*', test, name='ask'),                                         
-    url(r'^popular/.*', test, name='popular'),                                 
-    url(r'^new/.*', test, name='new')
-]  
-    
-    
+    url(r'^$', question_list_main),
+    url(r'^login/.*$', test, name='login'),
+    url(r'^signup/.*', test, name='signup'),
+    url(r'^question/(?P<id>[0-9]+)/$', question_details, name='question_details'),
+    url(r'^ask/.*', test, name='ask'),
+    url(r'^popular/$', question_list_main, name='popular'),
+    url(r'^new/$', test, name='new'),
+]
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,33 +1,31 @@
 from django.http import HttpResponse
 from qa.models import Question
 from django.core.paginator import Paginator
-from django.core.urlresolvers import reverse
+from django.shortcuts import render, get_object_or_404
 
- 
+
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
-def new(request):
-    qs = Question.objects.all()
-    qs = qs.order_by('-added_at')
-    page, paginator = paginate(request, qs)
-    paginator.baseurl = reverse('question_list') + '?page='
 
-    return render(request, 'list.html', {
-        'questions': page.object_list,
-        'page': page,
-        'paginator': paginator,
-    })
+def question_list(request):
+  page = request.GET.get('page', 1)
+  if request.path == '/popular/':
+    questions = Question.objects.popular()
+  else:
+    questions = Question.objects.new()
+
+  paginatior = Paginator(questions, 10)
+  page = paginator.page(page)
+
+  return render(request, 'qa/templates/question_list.html', {
+    'questions': page.object_list,
+    'page': page
+  })
 
 
-def popular(request):
-    qs = Question.objects.all()
-    qs = qs.order_by('-rating')
-    page, paginator = paginate(request, qs)
-    paginator.baseurl = reverse('popular') + '?page='
-
-    return render(request, 'list_rating.html', {
-        'questions': page.object_list,
-        'page': page,
-        'paginator': paginator,
-    })
+def question_details(request, id):
+  question = get_object_or_404(Question, id=id)
+  return render(request, 'qa/templates/question_details.html', {
+    'question': question
+  })
